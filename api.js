945097f2edcdea997bfd51bb38dcf88c79a92934
M7mdxzxa <129904682+M7mdxzxa@users.noreply.github.com>
2025-02-26 10:22:34 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_KEY = 'your api key';
+const API_KEY = '7Chzu328q9FYE1918xg_G11tY_GUv_7X_48pXhFYImQ';
 const API_BASE_URL = 'https://anime.apiclub.site';
 
 // تصدير المتغيرات للاستخدام في الملفات الأخرى
